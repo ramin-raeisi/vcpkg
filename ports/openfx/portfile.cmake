821e34a4560f6fc1d29ec48b5d3061f1456dc407
Nastihan BeheshtyPour <nastihanb@gmail.com>
2024-12-01 15:25:51 +0330

    Fix dependency managment when exporting

--- ports/openfx/portfile.cmake
+++ ports/openfx/portfile.cmake
@@ -11,13 +11,16 @@ vcpkg_from_github(
 
 file(COPY ${CMAKE_CURRENT_LIST_DIR}/CMakeLists.txt DESTINATION ${SOURCE_PATH})
 
-vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH})
+vcpkg_cmake_configure(SOURCE_PATH ${SOURCE_PATH}
+OPTIONS -DOPENFX_CONFIG_TEMPLATE_PATH=${CURRENT_PORT_DIR}/Config.cmake.in
+)
 vcpkg_cmake_install()
 vcpkg_fixup_pkgconfig()
 vcpkg_cmake_config_fixup(PACKAGE_NAME unofficial-openfx)
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/include/DocSrc")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 configure_file("${CMAKE_CURRENT_LIST_DIR}/usage" "${CURRENT_PACKAGES_DIR}/share/${PORT}/usage" COPYONLY)
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/Support/LICENSE")
